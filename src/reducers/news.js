c55f6c344d15a7c24455c026a105ce20e1e46a01
Canino Developments <ian.canino@gmail.com>
2018-08-29 10:18:51 -0400

    updated reducers to reflect changes made to actions

--- src/reducers/news.js
+++ src/reducers/news.js
@@ -1,13 +1,16 @@
-export default function newsReducer (state, action) {
+import { NEWS_REQUEST, NEWS_SUCCESS, NEWS_FAILURE } from '../constants'
+const initialState = {
+  collection: []
+}
+export default function news (state = initialState, action) {
   switch (action.type) {
-    case 'LOADING_NEWS':
-      return Object.assign({}, state, {loading: true})
-    case 'FETCH_NEWS':
-      return {loading: false, collection: action.payload}
+    case NEWS_REQUEST:
+      return { ...state, loading: true }
+    case NEWS_FAILURE:
+      return { ...state, loading: false, errorMessage: action.error }
+    case NEWS_SUCCESS:
+      return { ...state, loading: false, collection: action.payload }
     default:
-      return {
-        loading: false,
-        collection: []
-      }
+      return state
   }
 }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,26 +1,26 @@
-import { 
-  USERSLIST_SUCCESS, USERSLIST_REQUEST, 
-  USER_REQUEST, USER_SUCCESS
-} from 'constants'
+import {
+  USERSLIST_SUCCESS, USERSLIST_REQUEST, USERSLIST_FAILURE,
+  USER_REQUEST, USER_SUCCESS, USER_FAILURE
+} from '../constants'
 
-export default function userReducer (state = { user: '' }, action) {
-  switch (action.type) {
-    case USER_REQUEST:
-      return Object.assign({}, state, {loading: true})
-    case USER_SUCCESS:
-      return {...state, loading: false, user: action.payload}
-      default: 
-    return state
+const initialState = {
+  usersList: [],
+  loggedInUser: {
+    id: null,
+    name: null
   }
 }
-
-export function usersReducer (state = { list: [] }, action) {
-  switch(action.type) {
-    case USERSLIST_REQUEST:
-      return Object.assign({}, state, {loading: true})
+export default function users (state = initialState, action) {
+  switch (action.type) {
+    case USER_REQUEST || USERSLIST_REQUEST:
+      return { ...state, loading: true }
+    case USER_FAILURE || USERSLIST_FAILURE:
+      return { ...state, loading: false, errorMessage: action.error }
+    case USER_SUCCESS:
+      return { ...state, loading: false, loggedInUser: action.payload }
     case USERSLIST_SUCCESS:
-      return {...state, loading: false, list: [...action.payload]}
-      default: 
-    return state
+      return { ...state, loading: false, usersList: action.payload }
+    default:
+      return state
   }
 }
