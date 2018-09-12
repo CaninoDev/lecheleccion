832309de4c2c331310dc5ec39ca8392726755e09
Canino Developments <ian.canino@gmail.com>
2018-09-11 20:22:04 -0400

    removed use of iframe due to CORS limitation

--- src/components/modals/ArticleModal.js
+++ src/components/modals/ArticleModal.js
@@ -1,108 +1,65 @@
 import React, { Component } from 'react'
-import Iframe from 'react-iframe'
-import { AppBar, Toolbar } from '@material-ui/core'
-import { withStyles } from '@material-ui/core/styles'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import CloseIcon from '@material-ui/icons/Close'
 import Dialog from '@material-ui/core/Dialog'
 import DialogContent from '@material-ui/core/DialogContent'
-import DialogTitle from '@material-ui/core/DialogTitle'
 import Slide from '@material-ui/core/Slide'
+import { DialogContentText, DialogActions } from '@material-ui/core'
+import IconButton from '@material-ui/core/IconButton'
+import CloseIcon from '@material-ui/icons/Close'
 import ThumbDownAlt from '@material-ui/icons/ThumbDownAlt'
 import ThumbUpAlt from '@material-ui/icons/ThumbUpAlt'
+import Portal from '@material-ui/core/Portal'
 
 function Transition (props) {
   return <Slide direction='up' {...props} />
 }
-const styles = {
-  root: {
-    flex: 1,
-    alignItems: 'center'
-  },
-  container: {
-    display: 'flex',
-    flexWrap: 'nowrap'
-  },
-  flex: {
-    flex: 1
-  },
-  appBar: {
-    position: 'fixed',
-    height: 24
-  },
-  dialog: {
-    scroll: 'paper',
-    height: '95%',
-    width: '95%'
-  },
-  dialogPaper: {
-    minHeight: '90vh',
-    maxHeight: '90vh',
-    minWidth: '110vh',
-    maxWidth: '110vh'
-  }
-}
 
 class ArticleModal extends Component {
   render () {
-    const { isOpen, url, title, classes, closeModal } = this.props
+    const { isOpen, title, body, voteAndCloseModal } = this.props
     return (
-      <div className={classes.root}>
+      <div>
         <Dialog
+          onClose={voteAndCloseModal}
           open={isOpen}
           TransitionComponent={Transition}
           keepMounted
-          onClose={closeModal}
-          classes={{ paper: classes.dialogPaper }}
         >
-          <DialogTitle>
-            <AppBar>
-              <Toolbar>
-                <Typography
-                  variant='subheading'
-                  color='inherit'
-                >
-                  {title}
-                </Typography>
-                <IconButton
-                  color='inherit'
-                  onClick={() => closeModal(1)}
-                  id='upVote'
-                >
-                  <ThumbUpAlt />
-                </IconButton>
-                <IconButton
-                  color='inherit'
-                  onClick={() => closeModal(0)}
-                  id='downVote'
-                >
-                  <ThumbDownAlt />
-                </IconButton>
-                <IconButton
-                  color='inherit'
-                  onClick={() => closeModal(-1)}
-                  id='close'
-                  aria-label='Close'
-                >
-                  <CloseIcon />
-                </IconButton>
-              </Toolbar>
-            </AppBar>
-          </DialogTitle>
           <DialogContent>
-            <Iframe
-              align='center'
-              position='relative'
-              url={url}
-              width='80%'
-              height='80%'
-              display='initial'
-            />
+            <h1>{title}</h1>
+            <h2>{body}</h2>
           </DialogContent>
+          <DialogActions>
+            <IconButton
+              color='inherit'
+              onClick={() => voteAndCloseModal(1)}
+              id='upVote'
+            >
+              <ThumbUpAlt />
+            </IconButton>
+            <IconButton
+              color='inherit'
+              onClick={() => voteAndCloseModal(0)}
+              id='downVote'
+            >
+              <ThumbDownAlt />
+            </IconButton>
+            <IconButton
+              color='inherit'
+              onClick={() => voteAndCloseModal(-1)}
+              id='close'
+              aria-label='Close'
+            >
+              <Portal container={this.container}>
+                <DialogContentText position='fixed' variant='headline' gutterBottom>{title}</DialogContentText>
+                <DialogContentText variant='body2'>{body}</DialogContentText>
+              </Portal>
+              <CloseIcon />
+            </IconButton>
+          </DialogActions>
         </Dialog>
       </div>
     )
   }
 }
-export default withStyles(styles)(ArticleModal)
+
+export default ArticleModal
