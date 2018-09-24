2b77dd9fb96970aaadb912bcd542eb7d63b50bb4
Canino Developments <ian.canino@gmail.com>
2018-09-23 22:17:59 -0400

    restructured to trigger the actioncable via compoonentdidmount

--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { ErrorPage, NewsCardsGrid } from 'components'
+import { NewsCardsGrid } from '../components'
 import { ActionCable } from 'react-actioncable-provider'
 import Grid from '@material-ui/core/Grid'
 
@@ -12,85 +11,40 @@ class NewsContainer extends Component {
     }
   }
 
-  static async triggerAction () {
-    try {
-      const response = await fetch('/api/articles')
-      if (!response.ok) {
-        throw (response.json())
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   componentDidMount () {
-    NewsContainer.triggerAction()
+    this.requestRecentArticles()
   }
 
-  handleReceivedArticles = response => {
+  onReceived = (response) => {
+    console.log(`response.article: ${response.article}`)
     const { article } = response
     const { articles } = this.state
     this.setState({
       articles: [...articles, article]
     })
   }
-  // render () {
-  //   const {article, isLoading, errorMessage, query, selectedNews} = this.props
-  //   console.log(`query[0]: ${query[0]}, length: ${query.length}`)
-  //
-  //   function renderComponent () {
-  //     if (errorMessage !== null) {
-  //       return (
-  //         <ErrorPage />
-  //       )
-  //     } else if (isLoading === true && query.length < 1) {
-  //       return (
-  //         <div style={{ margin: { top: '400', left: '500px' } }}>
-  //           <CircularProgress />
-  //         </div>
-  //       )
-  //     } else if (query.length > 0) {
-  //       /* The following regular expression will match all words in whatever order they may appear in a given string */
-  //       const str = query.map((word) => (`(?=.*\\b${word}\\b)`))
-  //       const regexp = new RegExp(`${str.join('')}`, 'gi')
-  //
-  //       /* filter the current collection by the query terms */
-  //       let collection = news.filter((atomos) => (atomos.body.match(regexp) !== null))
-  //
-  //       /* check to see if the new search query from remote api made it through */
-  //       if (isLoading === false) {
-  //         collection = collection.concat(selectedNews)
-  //       }
-  //
-  //       return (
-  //         <NewsCardsGrid news={collection} />
-  //       )
-  //     } else if (isLoading === false && errorMessage === null && article && query.length === 0) {
-  //       return (
-  //         <NewsCardsGrid data={article} />
-  //       )
-  //     }
-  //   }
+
+  requestRecentArticles = () => {
+    this.refs.articlesCable.perform('recent', null)
+  }
+
+  componentDidMount () {
+    this.requestRecentArticles()
+  }
   render () {
     const { articles } = this.state
     return (
       <Grid item xs={9}>
         <ActionCable
           channel={{ channel: 'ArticlesChannel' }}
-          onReceived={this.handleReceivedArticles}
+          onReceived={this.onReceived}
+          ref='articlesCable'
         />
-        {articles && <NewsCardsGrid news={articles} /> }
+        { (articles > 0) && <NewsCardsGrid news={articles} /> }
       </Grid>
     )
   }
 }
 
-const mapStateToProps = state => ({
-  article: state.news.data,
-  selectedNews: state.news.selectData,
-  isLoading: state.news.loading,
-  errorMessage: state.news.errorMessage,
-  query: state.news.query
-})
 
-export default connect(mapStateToProps, null)(NewsContainer)
+export default NewsContainer
